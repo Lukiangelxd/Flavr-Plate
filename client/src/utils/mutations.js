102a047d046bf8e9a871a5243fcdff4b1637f7f4
YJRose <yj.lin963320@gmail.com>
2023-11-06 19:41:57 -0500

    added main.jax, app.css and initial mutation and queries.js

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,27 @@
+import { gql } from '@apollo/client';
+
+export const LoginUser = gql`
+    mutation login($email: String!, $password: String!) {
+        login(email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+                email
+            }
+        }
+    }
+    `;
+export const CreateUser = gql`
+    mutation addUser($username: String!, $email: String!, $password: String!) {
+        addUser(username: $username, email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+                email
+            }
+        }
+    }
+    `;
+
