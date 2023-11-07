8264bc5cf9de61c2ecb87c5e45582f0d1585fffd
Hannah Mcevoy <lukiangel57@gmail.com>
2023-11-06 20:40:32 -0500

    all queries

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,20 +1,68 @@
 import { gql } from '@apollo/client';
 
-// export const GET_ME = gql`
-//     query me {
-//         me {
-//             _id
-//             username
-//             email
-//             bookCount
-//             savedBooks {
-//                 bookId
-//                 authors
-//                 description
-//                 title
-//                 image
-//                 link
-//             }
-//         }
-//     }
-// `;
+export const GET_RECIPES = gql`
+  query getRecipes {
+    recipes {
+      _id
+      title
+      description
+      ingredients
+      instructions
+      category {
+        _id
+        name
+      }
+      author {
+        _id
+        username
+      }
+      createdAt
+    }
+  }
+`;
+
+export const GET_SINGLE_RECIPE = gql`
+  query getSingleRecipe($recipeId: ID!) {
+    recipe(recipeId: $recipeId) {
+      _id
+      title
+      description
+      ingredients
+      instructions
+      category {
+        _id
+        name
+      }
+      author {
+        _id
+        username
+      }
+      createdAt
+    }
+  }
+`;
+
+export const GET_CATEGORIES = gql`
+  query getCategories {
+    categories {
+      _id
+      name
+    }
+  }
+`;
+
+
+export const GET_USER = gql`
+  query getUser($username: String!) {
+    user(username: $username) {
+      _id
+      username
+      email
+      recipes {
+        _id
+        title
+        createdAt
+      }
+    }
+  }
+`;
