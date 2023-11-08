8dec4f5db36d70a6c210610f5dbd7b5bf12b6860
YJRose <yj.lin963320@gmail.com>
2023-11-08 18:20:04 -0500

    update Header.jsx

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,6 +1,4 @@
 import * as React from 'react';
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
 import { Login } from '../utils/mutations';
@@ -13,10 +11,7 @@ const Header = () => {
             <header>
                 <h1>Flavr Plate</h1>
                 <h2>Search for your favorite recipes</h2>
-                <Box component="form" sx={{ '& > :not(style)': { m: 1, width: '25ch' }, }} noValidate autoComplete="off">
-                    <TextField fullWidth label="Search" id="Search" />
-                </Box>
-                <Stack direction="row" spacing={2}>
+                 <Stack direction="row" spacing={2}>
                     <Button variant="contained">Login</Button>
                     <Button variant="contained">Signup</Button>
                 </Stack>
@@ -27,10 +22,7 @@ const Header = () => {
             <header>
                 <h1>Flavr Plate</h1>
                 <h2>Search for your favorite recipes</h2>
-                <Box component="form" sx={{ '& > :not(style)': { m: 1, width: '25ch' }, }} noValidate autoComplete="off">
-                    <TextField fullWidth label="Search" id="Search" />
-                </Box>
-                <Stack direction="row" spacing={2}>
+                 <Stack direction="row" spacing={2}>
                     <Button variant="contained">Profile</Button>
                     <Button variant="contained" disabled >Signup</Button>
                 </Stack>
