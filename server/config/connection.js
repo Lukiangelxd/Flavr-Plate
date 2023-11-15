0a94610b5d4d56bc7df9ab88605cea0914e29081
zach <zachhoover126@yahoo.com>
2023-11-15 16:54:54 -0500

    got it to connect to atlas database is empty now but everyones data will be connected from now on

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,6 @@
+console.log('MONGODB_URI:', process.env.MONGODB_URI);
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/mern-shopping');
+mongoose.connect('mongodb+srv://5dDvHjO0Q5ag5rGt:Mcarthur126@cluster0.dvepbo2.mongodb.net/flavr-plate_DB?retryWrites=true&w=majority');
 
 module.exports = mongoose.connection;
