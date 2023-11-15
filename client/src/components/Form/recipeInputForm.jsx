2bf03070a2bf13b2bb2a572813ca847f816e9e2f
Giselle <martinezgiselle5@gmail.com>
2023-11-14 19:48:00 -0500

    made changes to recipe form, removed values, added correct structure to recipe content, added divs

--- client/src/components/Form/recipeInputForm.jsx
+++ client/src/components/Form/recipeInputForm.jsx
@@ -1,123 +1,131 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { useMutation } from '@apollo/client';
-import { CreateRecipe, GET_RECIPES } from './utils/mutations';
-import {Box, TextField, MenuItem} from '@mui/material';
+import { CreateRecipe, GET_RECIPES } from '../../utils/mutations';
+import { Box, TextField, MenuItem } from '@mui/material';
 
 
 
 const recipeForm = () => {
-  const [recipeData, setRecipeData] = useState({
-    name: '',
-    description: '',
-    image: '',
-    instructions: '',
-    category: '', 
-  });
+    const [recipeData, setRecipeData] = useState({
+        name: "",
+        description: "",
+        image: "",
+        instructions: "",
+        category: [
+            { name: "" }
+        ],
+        ingredients: [
+            { name: "", amount: "" },
+            { name: "", amount: "" },
+            { name: "", amount: "" },
+        ],
+    });
 
-  const [addRecipe, { error }] = useMutation(CreateRecipe);
+    const [addRecipe, { error }] = useMutation(CreateRecipe);
 
-  const handleInputChange = (event) => {
-    const { name, value } = event.target;
-    setRecipeData({ ...recipeData, [name]: value });
-  };
+    const handleInputChange = (event) => {
+        const { name, value } = event.target;
+        setRecipeData({ ...recipeData, [name]: value });
+    };
 
-  const handleFormSubmit = async (event) => {
-    event.preventDefault();
+    const handleFormSubmit = async (event) => {
+        event.preventDefault();
 
-    try {
-      
-      await addRecipe({
-        variables: { recipeData },
-        refetchQueries: [{ query: GET_RECIPES }],
-      });
+        try {
 
-      
-      setRecipeData({
-        name: '',
-        description: '',
-        image: '',
-        instructions: '',
-        category: '',
-      });
-    } catch (err) {
-      console.error(err);
-    }
-  };
+            await addRecipe({
+                variables: { recipeData },
+                refetchQueries: [{ query: GET_RECIPES }],
+            });
 
-  return (
-    <Box
-      component="form"
-      sx={{
-        '& .MuiTextField-root': { m: 1, width: '25ch' },
-        display: 'flex',
-        flexDirection: 'column',
-      }}
-      noValidate
-      autoComplete="off"
-      onSubmit={handleFormSubmit}
-    >
-      <TextField
-        id="outlined-name"
-        label="Name"
-        variant="outlined"
-        name="name"
-        value={recipeData.name}
-        onChange={handleInputChange}
-      />
 
-      <TextField
-        id="outlined-description"
-        label="Description"
-        multiline
-        rows={4}
-        variant="outlined"
-        name="description"
-        value={recipeData.description}
-        onChange={handleInputChange}
-      />
+            setRecipeData({
+                name: "",
+                description: "",
+                image: "",
+                instructions: "",
+                category: [
+                    { name: "" }
+                ],
+                ingredients: [
+                    { name: "", amount: "" },
+                    { name: "", amount: "" },
+                    { name: "", amount: "" },
+                ],
+            });
+        } catch (err) {
+            console.error(err);
+        }
+    };
 
-      <TextField
-        id="outlined-image"
-        label="Image URL"
-        variant="outlined"
-        name="image"
-        value={recipeData.image}
-        onChange={handleInputChange}
-      />
+    return (
+        <Box
+            component="form"
+            sx={{
+                '& .MuiTextField-root': { m: 1, width: '25ch' },
+                display: 'flex',
+                flexDirection: 'column',
+            }}
+            noValidate
+            autoComplete="off"
+            onSubmit={handleFormSubmit}
+        >
+            <div>
+                <TextField
+                    id="outlined-name"
+                    label="Name"
+                    variant="outlined"
+                    name="name"
+                    onChange={handleInputChange}
+                />
+            </div>
+            <div>
+                <TextField
+                    id="outlined-description"
+                    label="Description"
+                    multiline
+                    rows={4}
+                    variant="outlined"
+                    name="description"
+                    onChange={handleInputChange}
+                />
+            </div>
+            <div>
+                <TextField
+                    id="outlined-image"
+                    label="Image URL"
+                    variant="outlined"
+                    name="image"
+                    onChange={handleInputChange}
+                />
+            </div>
+            <div>
+                <TextField
+                    id="outlined-instructions"
+                    label="Instructions"
+                    multiline
+                    rows={4}
+                    variant="outlined"
+                    name="instructions"
+                    onChange={handleInputChange}
+                />
+            </div>
+            <div>
+                <TextField
+                    id="outlined-custom-category"
+                    label="Custom Category"
+                    variant="outlined"
+                    name="category"
+                    onChange={handleInputChange}
+                />
+            </div>
+            <Button type="submit" variant="contained" color="primary">
+                Submit
+            </Button>
 
-      <TextField
-        id="outlined-instructions"
-        label="Instructions"
-        multiline
-        rows={4}
-        variant="outlined"
-        name="instructions"
-        value={recipeData.instructions}
-        onChange={handleInputChange}
-      />
-
-      <TextField
-        id="outlined-select-category"
-        select
-        label="Select Category"
-        variant="outlined"
-        name="category"
-        value={recipeData.category}
-        onChange={handleInputChange}
-      >
-        
-        <MenuItem value="breakfast">breakfast</MenuItem>
-        <MenuItem value="lunch">lunch</MenuItem>
-        <MenuItem value="dinner">dinner</MenuItem>
-      </TextField>
-
-      <Button type="submit" variant="contained" color="primary">
-        Submit
-      </Button>
-
-      {error && <p>Error: {error.message}</p>}
-    </Box>
-  );
+            {error && <p>Error: {error.message}</p>}
+        </Box>
+    );
 };
 
 export default recipeForm;
