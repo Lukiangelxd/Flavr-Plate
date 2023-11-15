b18f54fea4b13243200baaa0eadb4093856128d1
Hannah Mcevoy <lukiangel57@gmail.com>
2023-11-14 19:42:11 -0500

    done

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -5,7 +5,6 @@ import MenuIcon from '@mui/icons-material/Menu';
 import '../Header/Header.css';
 import Auth from '../../utils/auth';
 import flavrPlate from '../../assets/Flavr Plate logo.jpg';
-import flavrPlate from '../../assets/Flavr Plate logo.jpg';
 
 function Header() {
   const [anchorEl, setAnchorEl] = React.useState(null);
@@ -23,64 +22,6 @@ function Header() {
     <AppBar position="static" style={{ backgroundColor: '#D8A79D', backdropFilter: 'blur(10px)' }}>
       <Toolbar style={{ display: 'flex', justifyContent: 'space-between' }}>
         <div style={{ display: 'flex', alignItems: 'center' }}>
-          <IconButton size="large" edge="start" color="inherit" aria-label="menu">
-          <img src={flavrPlate} alt="logo" className="logo" />
-  if (loggedIn) {
-    return (
-      <AppBar position="static" style={{ backgroundColor: '#D8A79D', backdropFilter: 'blur(10px)' }}>
-        <Toolbar>
-          <IconButton size="large" edge="start" color="inherit" aria-label="menu">
-            <img src={flavrPlate} alt="logo" className="logo" />
-          </IconButton>
-          <Typography variant="h6" component="div" className="company-name">
-            <span style={{ fontFamily: 'Segoe Script', fontSize: '32px', fontWeight: 'bold' }}>Flavr Plate</span>
-          </Typography>
-          <Stack direction="row" spacing={2} sx={{ marginLeft: 'auto' }}>
-            <Button
-              color="inherit"
-              href="/Profile"
-              sx={{
-                fontFamily: 'Segoe UI, sans-serif',
-                '&:hover': {
-                  backgroundColor: '#F7D1C3', // Lighter shade for hover
-                  transition: '0.3s',
-                  transform: 'scale(1.05)',
-                },
-                '&:active': {
-                  backgroundColor: '#C48172', // Darker shade for click
-                  transform: 'scale(0.95)',
-                },
-              }}
-            >
-              Profile
-            </Button>
-            <Button
-              color="inherit"
-              href="/"
-              sx={{
-                fontFamily: 'Segoe UI, sans-serif',
-                '&:hover': {
-                  backgroundColor: '#F7D1C3', // Lighter shade
-                  transition: '0.3s',
-                  transform: 'scale(1.05)',
-                },
-                '&:active': {
-                  backgroundColor: '#C48172', // Darker shade
-                  transform: 'scale(0.95)',
-                },
-              }}
-              onClick={() => Auth.logout()}
-            >
-              Logout
-            </Button>
-          </Stack>
-        </Toolbar>
-      </AppBar>
-    );
-  } else {
-    return (
-      <AppBar position="static" style={{ backgroundColor: '#D8A79D', backdropFilter: 'blur(10px)' }}>
-        <Toolbar>
           <IconButton size="large" edge="start" color="inherit" aria-label="menu">
             <img src={flavrPlate} alt="logo" className="logo" />
           </IconButton>
